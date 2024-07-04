87fbcebc465cef1cd312c784e59298fb4baf3e45
KJS <comujs@gmail.com>
2024-07-04 21:55:29 +0900

    first2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 테스트웹 <code>src/App.tsx</code> and save to reload.
+          Edit 테스트웹1 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
